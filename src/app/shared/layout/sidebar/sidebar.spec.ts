202ca351db57f21c3b4038fa0775e88b5e4d4d6f
bm-codeur <bmofficiel224@gmail.com>
2026-05-20 04:22:57 +0000

    feat: app layout with role-based sidebar

--- src/app/shared/layout/sidebar/sidebar.spec.ts
+++ src/app/shared/layout/sidebar/sidebar.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { Sidebar } from './sidebar';
 
@@ -8,7 +10,7 @@ describe('Sidebar', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Sidebar]
+      imports: [Sidebar, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
 
